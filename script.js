3992dbf1f8f3993f65bec0fa5af224cf65d2b4b9
Raaz-009 <thameemraz829@gmail.com>
2024-01-23 23:00:36 +0530

    added remove btn

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const box=document.getElementById("box");
 const list=document.getElementById("list");
 
 function addtask(){
-    if(box.value==''){
+    const taskText=box.value.trim();
+    if(taskText==""){
         alert("write a task");
     }
     else{
-        let li=document.createElement("li");
-        li.innerHTML=box.value;
-        list.appendChild(li);
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+            <span>${taskText}</span>
+            <button onclick="removeTask(this)">Remove</button>
+        `;
+
+        // Add the new list item to the todo list
+        list.appendChild(listItem);
+
+        // Clear the input field
+        box.value="";
     }
+    
+}
+
+function removeTask(button) {
+    const listItem = button.parentNode;
+    list.removeChild(listItem);
 }
